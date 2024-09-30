4cb0008ae2c6be9c4e116f7f5a1a9c826325a677
Romain Miary <romainmiary12@gmail.com>
2024-09-30 10:51:04 +0200

    First puch

---
+++ config/cors.blade.php
@@ -0,0 +1,6 @@
+paths => ['api/*'],
+allowed_methods => ['*'],
+allowed_origins => ['*'], // Ou spécifie l'URL de ton frontend
+allowed_headers => ['*'],
+exposed_headers => [],
+supports_credentials => false,
